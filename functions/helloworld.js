8e06aea2577904461d22d12c668a5a43dd6de9b1
setaria <ahu430@hotmail.com>
2023-11-08 02:23:27 +0900

    test get---->post

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,4 +1,17 @@
-export const onRequestGet = async (context) => {
+export const onRequestGet = async () => {
+  const apiUrl = "https://jsonreader.pages.dev/helloworld";
+  fetch(apiUrl, {
+    method: 'POST',
+    body: 'https://misskey.gamelore.fun/storage/files/6cff3461-2293-4f83-959f-f89d1e32aba5',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then(response => response.text())
+  .then(value => {
+    console.log(value);
+  })
+  .catch(error => console.error('通信に失敗しました', error));
   return new Response("Hello, world!")
 }
 
--- script.js
+++ script.js
@@ -101,14 +101,7 @@ function setData(notes) {
 
 
 const apiUrl = "https://jsonreader.pages.dev/helloworld";
-fetch(apiUrl, {
-  method: 'POST',
-  body: 'https://jsonreader.pages.dev/helloworld',
-  // body: 'https://misskey.gamelore.fun/storage/files/6cff3461-2293-4f83-959f-f89d1e32aba5',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
+fetch(apiUrl)
 .then(response => response.text())
 .then(value => {
   console.log(value);
