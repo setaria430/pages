0a2ec6a629ab24aefab4562bbfb53e64d2b5a53b
setaria <ahu430@hotmail.com>
2023-11-08 02:39:02 +0900

    test nuaaaaaaaa

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,12 +1,6 @@
 export const onRequestGet = async () => {
-  const apiUrl = "https://jsonreader.pages.dev/helloworld";
-  fetch(apiUrl, {
-    method: 'POST',
-    body: 'https://misskey.gamelore.fun/storage/files/6cff3461-2293-4f83-959f-f89d1e32aba5',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+  const apiUrl = "https://misskey.gamelore.fun/storage/files/6cff3461-2293-4f83-959f-f89d1e32aba5";
+  fetch(apiUrl)
   .then(response => response.text())
   .then(value => {
     console.log(value);
