4de8b79f7cec73699b68e7101e61de0ee309eb91
setaria <ahu430@hotmail.com>
2023-11-08 02:16:54 +0900

    test post--->get

--- script.js
+++ script.js
@@ -103,7 +103,8 @@ function setData(notes) {
 const apiUrl = "https://jsonreader.pages.dev/helloworld";
 fetch(apiUrl, {
   method: 'POST',
-  body: 'https://misskey.gamelore.fun/storage/files/6cff3461-2293-4f83-959f-f89d1e32aba5',
+  body: 'https://jsonreader.pages.dev/helloworld',
+  // body: 'https://misskey.gamelore.fun/storage/files/6cff3461-2293-4f83-959f-f89d1e32aba5',
   headers: {
     'Content-Type': 'application/json',
   },
